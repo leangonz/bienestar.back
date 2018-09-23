275f2c6994df5d55d2948889321099de604a5b1a
leangonz <leo-2590@hotmail.com>
2018-09-23 16:05:58 -0300

    cors

--- src/main/java/ungs/bienestar/back/controller/ComboController.java
+++ src/main/java/ungs/bienestar/back/controller/ComboController.java
@@ -3,6 +3,7 @@ package ungs.bienestar.back.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ public class ComboController {
 	@Autowired
 	private ComboService comboService;
 	
+	@CrossOrigin(origins = "*")
 	@RequestMapping("/comboMenue")
     public List<ComboDto> comboMenue() {
         return comboService.menuesItems();
