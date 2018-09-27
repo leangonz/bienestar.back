34f2865dd3dc8e7a0d8450875a8e6499b18e9e87
leangonz <leo-2590@hotmail.com>
2018-09-26 21:39:56 -0300

    recibo un menu realizado, todavia no se persiste

--- src/main/java/ungs/bienestar/back/controller/CargaInsumoController.java
+++ src/main/java/ungs/bienestar/back/controller/CargaInsumoController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import ungs.bienestar.back.dto.InsumoDto;
+import ungs.bienestar.back.dto.InsumoRealizadoDto;
 import ungs.bienestar.back.service.InsumosService;
 
 @RestController
@@ -28,5 +29,12 @@ public class CargaInsumoController {
 	@GetMapping("/cargaInsumo")
 	public List<InsumoDto> cargaInsumo() {
 		return insumoService.obtenerInsumos();
+	}
+	
+	@CrossOrigin(origins = "*")
+	@PostMapping("/guardarInsumoRealizado")
+	public Boolean guardarInsumoRealizado(@RequestBody InsumoRealizadoDto dto){
+		System.out.println();
+		return true;
 	}
 }
---
+++ src/main/java/ungs/bienestar/back/dto/InsumoRealizadoDto.java
@@ -0,0 +1,65 @@
+package ungs.bienestar.back.dto;
+
+import java.util.Date;
+import java.util.List;
+
+public class InsumoRealizadoDto {
+
+	private Date fecha;
+	private Integer lactarios;
+	private Integer unAnio;
+	private Integer dosAnios;
+	private Integer tresAnios;
+	private Integer cuatroCincoAnios;
+	private Integer adultos;
+	private List<InsumoDto> insumos;
+	
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public Integer getLactarios() {
+		return lactarios;
+	}
+	public void setLactarios(Integer lactarios) {
+		this.lactarios = lactarios;
+	}
+	public Integer getUnAnio() {
+		return unAnio;
+	}
+	public void setUnAnio(Integer unAnio) {
+		this.unAnio = unAnio;
+	}
+	public Integer getDosAnios() {
+		return dosAnios;
+	}
+	public void setDosAnios(Integer dosAnios) {
+		this.dosAnios = dosAnios;
+	}
+	public Integer getTresAnios() {
+		return tresAnios;
+	}
+	public void setTresAnios(Integer tresAnios) {
+		this.tresAnios = tresAnios;
+	}
+	public Integer getCuatroCincoAnios() {
+		return cuatroCincoAnios;
+	}
+	public void setCuatroCincoAnios(Integer cuatroCincoAnios) {
+		this.cuatroCincoAnios = cuatroCincoAnios;
+	}
+	public Integer getAdultos() {
+		return adultos;
+	}
+	public void setAdultos(Integer adultos) {
+		this.adultos = adultos;
+	}
+	public List<InsumoDto> getInsumos() {
+		return insumos;
+	}
+	public void setInsumos(List<InsumoDto> insumos) {
+		this.insumos = insumos;
+	}
+}
